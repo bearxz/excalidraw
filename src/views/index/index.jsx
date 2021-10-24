9c4a3c7fd17f6129af3ca88cc275288d14b1bfa9
xiongzhen <2454037954@qq.com>
2021-10-24 20:05:22 +0800

    feat: 完成excalidraw保存功能

--- src/views/index/index.jsx
+++ src/views/index/index.jsx
@@ -1,20 +1,32 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef, useMemo } from "react";
+import { getCurrentWidgetId } from '../../common/utils';
 import Excalidraw, {
   // exportToCanvas,
   // exportToSvg,
   // exportToBlob,
   languages,
+  serializeAsJSON,
 } from "@excalidraw/excalidraw";
+// import { serializeAsJSON } from '@excalidraw/excalidraw';
 import { Icon } from 'react-kui';
 import initialData from "../../common/initialData";
+import { uploadFile, downloadFile, setWidgetAttr, getWidgetAttr } from "../../net/net_api";
 import "./index.css";
 
+const getDate = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDay();
+  return `${year}-${month}-${day}_${Date.now()}`;
+};
 const renderTopRightUI = (theme, grid, view, {
   onLanguage, // 设置语言
   setTheme, // 设置主题
   setGrid, // 设置网格
   setView, // 设置视图模式
   setZen, // 禅模式
+  saveFile,
 }) => {
   return (
     <div 
@@ -60,6 +72,12 @@ const renderTopRightUI = (theme, grid, view, {
             grid ? <Icon type="grid" size="18" /> : <Icon type="grid-outline" size="18" />
           }
         </div>
+        <div onClick={() => {
+          // TODO: 归档保存
+          saveFile();
+        }}>
+          <Icon type="file-tray-full-outline" size="18" color="#f00" />
+        </div>
       </div>
     </div>
   );
@@ -82,36 +100,60 @@ export default function Index() {
   // 语言设置
   const [lang, setLang] = useState(navigator.language);
 
+  // 获取挂件块ID
+  const currentWidgetId = useMemo(() => getCurrentWidgetId(), []);
 
-  useEffect(() => {
-    const onHashChange = () => {
-      const hash = new URLSearchParams(window.location.hash.slice(1));
-      const libraryUrl = hash.get("addLibrary");
-      if (libraryUrl) {
-        excalidrawRef.current.importLibrary(libraryUrl, hash.get("token"));
-      }
-    };
-    window.addEventListener("hashchange", onHashChange, false);
-    return () => {
-      window.removeEventListener("hashchange", onHashChange);
-    };
-  }, []);
+  async function saveExcalidrawFile() {
+    const fileBlob = new Blob([serializeAsJSON(excalidrawRef.current.getSceneElements(), excalidrawRef.current.getAppState())], {
+      type: 'application/json'
+    });
+    const fileName = currentWidgetId + '.excalidraw';
+    const file = new File([fileBlob], fileName);
+    const res = await uploadFile(file);
+    const fileMap = res.succMap[fileName];
+    // 获取返回后的地址，保存在挂件块的custom-excalidraw属性上
+    setWidgetAttr(currentWidgetId, {
+      "custom-excalidraw": fileMap,
+    });
+    excalidrawRef.current.setToastMessage('保存成功');
+  }
+
   useEffect(() => {
     console.log('感谢的Excalidraw和React KUI的支持！');
     console.log('author: bearxz & github:https://github.com/bearxz! ');
-  }, []);
+    async function getExcalidrawFile() {
+      // 获取挂件的custom-excalidraw属性
+      const data = await getWidgetAttr(currentWidgetId);
+      const excalidrawFileName = data['custom-excalidraw'];
+      if(!excalidrawFileName) {
+        return;
+      }
+      // 获取文件内容
+      downloadFile(excalidrawFileName).then(res => {
+        if (!res) {
+          return;
+        }
+        setTimeout(() => {
+          excalidrawRef.current.updateScene(res);
+        }, 1000);
+      })
+    }
+    getExcalidrawFile();
+  }, [currentWidgetId]);
+
   return (
     <div className="App">
-        <div className="excalidraw-wrapper">
+        <div className="excalidraw-wrapper" onBlur={() => {
+        }}>
           <Excalidraw
             ref={excalidrawRef}
             initialData={initialData}
-            onLibraryChange={() => {console.log('hello woeld')}}
+            // onLibraryChange={() => {console.log('hello woeld')}}
             viewModeEnabled={viewModeEnabled}
             zenModeEnabled={zenModeEnabled}
             gridModeEnabled={gridModeEnabled}
             theme={theme}
-            name="Custom name of drawing"
+            name={getDate()}
             UIOptions={{ canvasActions: { 
               loadScene: true,
               theme: true,
@@ -126,11 +168,12 @@ export default function Index() {
                 setGrid: () => setGridModeEnabled(!gridModeEnabled),
                 setView: () => setViewModeEnabled(!viewModeEnabled),
                 setZen: () => setZenModeEnabled(!zenModeEnabled),
+                saveFile: () => saveExcalidrawFile(),
               }
             )}
             renderFooter={renderFooter}
             langCode={lang}
-            autoFocus={true}
+            autoFocus={false}
             detectScroll={true}
           />
         </div>
